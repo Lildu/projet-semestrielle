a8d75f4bebaac47d11ae766a608ade24ed9ba8ff
Lildu <lildci83@gmail.com>
2022-05-12 16:58:52 +0200

    maj

--- src/Player.js
+++ src/Player.js
@@ -4,57 +4,102 @@ class Player {
     constructor(scene) {
         this.scene=scene
         this.cameras=scene
-        this.player = this.scene.physics.add.sprite(50,  600, 'player');
-        this.player.setBounce(0.1);
+        this.player = this.scene.physics.add.sprite(50, -300,"dude");
+        //this.player.setBounce(0.1);
+        this.player.setScale(1);
+        this.player.setFlipX(true);
+
+
         this.player.setCollideWorldBounds(false);
         this.scene.physics.add.collider(this.player, this.scene.platforms);
 
         this.scene.anims.create({
             key: 'walk',
-            frames: this.scene.anims.generateFrameNames('player', {
-                prefix: 'robo_player_',
-                start: 2,
-                end: 3,
-            }),
-            frameRate: 10,
-            repeat: -1
+            frames: this.scene.anims.generateFrameNumbers('walk', {start: 0, end: 119}),
+            frameRate: 60,
+            flipX: true,
+            repeat: 1,
+
         });
+
         this.scene.anims.create({
             key: 'idle',
-            frames: [{key: 'player', frame: 'robo_player_0'}],
-            frameRate: 10,
+            frames: this.scene.anims.generateFrameNumbers('idle', {start: 0, end: 119}),
+            frameRate: 60,
+            flipX: true,
+            repeat: -1
 
         });
         this.scene.anims.create({
             key: 'jump',
-            frames: [{key: 'player', frame: 'robo_player_1'}],
-            frameRate: 10,
+            frames: this.scene.anims.generateFrameNumbers('walk', {start: 0, end: 119}),
+            frameRate: 60,
             repeat:-1,
-
         });
     }
 
     jump(){
         this.player.setVelocityY(-420);
         this.player.play('jump', true);
+
     }
-    moveRight(){
-        this.player.setVelocityX(300);
-        this.player.setFlipX(false);
-        if (this.player.body.onFloor()) {
-            this.player.play('walk', true)}
-    }
-    moveLeft(){
-        this.player.setVelocityX(-300);
+    moveRight(v){
+        this.player.setVelocityX(v);
+
         if (this.player.body.onFloor()) {
             this.player.play('walk', true)}
         this.player.setFlipX(true);
+    }
+    moveLeft(v){
+        this.player.setVelocityX(-v);
+        if (this.player.body.onFloor()) {
+            this.player.play('walk', true)}
+        this.player.setFlipX(false);
     }
     stop(){
         this.player.setVelocityX(0);
         if (this.player.body.onFloor()) {
             this.player.play('idle',true)
         }
+    }
+    tir(v){
+
+        this.balle = this.scene.add.sprite(this.player.x, this.player.y,"square");
+        this.balle.setDisplaySize(10,10);
+
+        this.scene.tweens.add({
+            targets: this.balle,
+            x: this.player.x+(15000*v),
+            duration: 10000,
+            ease: 'Power',
+            repeat: 0,
+            delay:0
+        });
+        if(this.balle.y < this.player.player.y-500 ){
+
+            this.balle.destroy();
+        }
+        if(this.balle.y > this.player.player.y+500 ){
+
+            this.balle.destroy();
+        }
+        if(this.balle.x < this.player.player.x-1000 ){
+
+            this.balle.destroy();
+        }
+        if(this.balle.x > this.player.player.x+1000 ) {
+
+            this.balle.destroy();
+        }
+    }
+
+
+    gravitystop(){
+
+        this.player.setGravityY(0);
+    }
+    gravitynorm(){
+        this.player.setGravityY(500);
     }
 
     }
--- src/scene.js
+++ src/scene.js
@@ -7,13 +7,24 @@ class scene extends Phaser.Scene {
         this.load.atlas('player', 'assets/images/kenney_player.png', 'assets/images/kenney_player_atlas.json');
         this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
 
+        this.load.image('dude', 'assets/images/dude.png');
+        this.load.image('square', 'assets/images/square.png');
+
+
+        this.load.spritesheet('walk', 'assets/images/tilesheet/tilesheet-walk.png',{ frameWidth: 512, frameHeight: 512 });
+        this.load.spritesheet('idle', 'assets/images/tilesheet/tilesheet-idle.png',{ frameWidth: 512, frameHeight: 512 });
+        this.load.spritesheet('shield', 'assets/images/tilesheet/tilesheet-shield.png',{ frameWidth: 512, frameHeight: 512 });
+        this.load.spritesheet('sword', 'assets/images/tilesheet/tilesheet-sword2.png',{ frameWidth: 512, frameHeight: 512 });
+        this.load.spritesheet('up', 'assets/images/tilesheet/tilesheet-gun-up.png',{ frameWidth: 512, frameHeight: 512 });
+        this.load.spritesheet('midle', 'assets/images/tilesheet/tilesheet-gun-midle.png',{ frameWidth: 512, frameHeight: 512 });
+        this.load.spritesheet('down', 'assets/images/tilesheet/tilesheet-gun-down.png',{ frameWidth: 512, frameHeight: 512 });
+
         // Load the export Tiled JSON
         this.load.tilemapTiledJSON('map', 'assets/tilemaps/Alpha1.json');
     }
 
 
     create() {
-
 
 
 
@@ -28,26 +39,80 @@ class scene extends Phaser.Scene {
         this.platforms.setCollisionByExclusion(-1, true);
         this.cursors = this.input.keyboard.createCursorKeys();
 
+        
 
-        this.player = new Player(this)
 
+
+        this.initKeyboard();
         this.cameras.main.startFollow(this.player.player,false);
     }
 
+    initKeyboard(){
+
+        this.player = new Player(this)
+        let me=this;
+        this.input.keyboard.on('keydown', function(kevent)
+        {
+            switch (kevent.keyCode)
+            {
+                case Phaser.Input.Keyboard.KeyCodes.E:
+                    console.log("oui")
+                    me.player.gravitystop();
+                    break;
+
+                case Phaser.Input.Keyboard.KeyCodes.A:
+                    console.log("oui")
+                    me.player.gravitynorm();
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.NUMPAD_SIX:
+                    console.log("oui")
+                    me.player.tir(1);
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.NUMPAD_FOUR:
+                    console.log("oui")
+                    me.player.tir(-1);
+                    break;
+
+
+
+            }
+        });
+        this.input.keyboard.on('keyup', function(kevent)
+        {
+            switch (kevent.keyCode){
+            }
+
+
+        });
+    }
 
     update() {
 
         switch (true) {
             case (this.cursors.space.isDown || this.cursors.up.isDown) && this.player.player.body.onFloor():
                 this.player.jump()
-                console.log("oui")
                 break;
             case this.cursors.left.isDown:
-                this.player.moveLeft()
+
+                if(this.cursors.left.shiftKey){
+                    this.player.moveLeft(600);
+                    break;
+                }
+                else{
+                    this.player.moveLeft(300);
+                }
                 break;
             case this.cursors.right.isDown:
-                this.player.moveRight();
+                if(this.cursors.right.shiftKey){
+                    this.player.moveRight(600);
+                    break;
+                }
+                else{
+                    this.player.moveRight(300);
+                }
                 break;
+
+
             default:
                 this.player.stop();
         }
